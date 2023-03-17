a6fa87cf2ad9ae868e30b7b7a36112b38cf88c1a
Quophi7 <quophi7@gmail.com>
2023-03-17 08:59:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  A program that prints out exact stuff
+ * Return: 0 (Excellent)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+i}
