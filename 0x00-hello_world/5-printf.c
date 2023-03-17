57cffefb1c047fec8bc13f19076245745d273a1c
Quophi7 <quophi7@gmail.com>
2023-03-17 10:28:28 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints exact stuff
+ * Return: 0 (Excellent)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
