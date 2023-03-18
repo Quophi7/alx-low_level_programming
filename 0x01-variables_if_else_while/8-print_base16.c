dc6564f123a13c9cd7ff73862b2bec9c6d78f20b
Quophi7 <quophi7@gmail.com>
2023-03-18 05:15:45 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all numbers of base sixteen in lowercase
+ * Retunr: 0 (Excellent)
+ */
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
