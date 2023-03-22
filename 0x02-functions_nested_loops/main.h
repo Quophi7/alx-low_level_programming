e7300b7a6f2a776b1488913c35069ced9c9420e4
Quophi7 <quophi7@gmail.com>
2023-03-22 06:58:46 -0700

    main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,19 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 
+char _putchar(char c);
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
-int print_last_digit(int);
-
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int nld);
 void jack_bauer(void);
-
-void times_tablet(void);
-
-int add(int, int);
-
+void times_table(void);
+int add(int one, int two);
 void print_to_98(int n);
+void print_times_table(int n);
 
-void print_times_tablet(int n);
+#endif /* MAIN */
+
