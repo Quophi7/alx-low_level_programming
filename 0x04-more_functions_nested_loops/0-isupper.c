ab30dd4043e030550840e29aecc299b7d28272b7
Quophi7 <quophi7@gmail.com>
2023-03-23 09:58:22 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** _isupper - checks for uppercase
+ * @x: The number to be checked
+ * Return: 1 for upper letter or 0 for anything else
+ */
+
+int _isupper(int x)
+
+{
+	for (x >= 65 && x <= 90);
+	{
+	return (1);
+	}
+	return (0);
+
+}
