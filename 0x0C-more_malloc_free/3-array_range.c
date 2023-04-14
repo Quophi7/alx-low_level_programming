524b721bc6bf939b820cf22d568486f544f33278
Quophi7 <quophi7@gmail.com>
2023-04-14 16:39:31 +0000

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,50 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <limits.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
  *
- * Return: Nothing.
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
  */
-
 int *array_range(int min, int max)
-
 {
-	unsigned int i;
+	int *ar;
+	int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf(" 0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
+	if (min > max)
+		return (NULL);
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
-int main(void)
-{
-	int *a;
+	if (ar == NULL)
+		return (NULL);
 
-	a = array_range(0, 10);
-	simple_print_buffer(a, 11);
-	free(a);
-	return (0);
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
+
+	return (ar);
 }
